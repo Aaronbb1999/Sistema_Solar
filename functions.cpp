81942e9ea92d95cbdda103e3e21c62e7dda50a7b
Aaronbb1999 <aaronbenitez2110@gmail.com>
2022-03-30 18:22:11 +0200

    Clase 30032022

--- functions.cpp
+++ functions.cpp
@@ -1,9 +1,12 @@
 #include <math.h>
 #include "functions.h"
-
+#include <stdio.h>
 const int MAXFILS=20; 
 const int MAXCOLS=20;
-const double G=6.67*Pow(10,-11);
+const double G=6.67E-11;
+const double c=1.496E11;
+const double Ms=1.99E30;
+
 
 void Reinitialize(double A[][MAXCOLS], int fils, int cols)
 {
@@ -18,31 +21,33 @@ void Reinitialize(double A[][MAXCOLS], int fils, int cols)
     }
 }
 
-double Mod1(double v[2])
+double Mod1(double v[1])
 {
     double M;
 
-    M=sqrt(v[0]*v[0]+v[1]*v[1]+v[2]*v[2]);
+    M=sqrt(v[0]*v[0]+v[1]*v[1]);
 
     return M;
 }
 
-void acel(double a[][2], double r[][2], double M[] ,int n)
+void acel(double a[][1], double r[][1], double M[] ,int n)
 {
     int i,j,k,i1;
     double R[MAXFILS][MAXCOLS][2];
     double A[MAXFILS][MAXCOLS][2];
-    double aux[2];
+    double aux[1];
     double mod;
 
-    Reinitialize(a,n,2);
+    //Reiniciamos la matriz de aceleraciones
+    Reinitialize(a,n,1);
 
     for(i=0;i<=n;i++)
      {
          for(j=0;j<=n;j++)
          {
-             for(k=0;k<=2;k++)
+             for(k=0;k<=1;k++)
              {
+                 //Hallamos las diferencias entre cada planeta y lo almacenamos en un tensor
                  R[i][j][k]=r[i][k]-r[j][k];
              }
          }
@@ -52,14 +57,15 @@ void acel(double a[][2], double r[][2], double M[] ,int n)
      {
          for(j=0;j<=n;j++)
          {
-
-             for(i1=0;i1<=2;i1++)
+             //Se halla el módulo del vector que une le planta i con el j
+             for(i1=0;i1<=1;i1++)
              {
                 aux[i1]=R[i][j][i1];
              }
              mod=Mod1(aux);
-             for(k=0;k<=2;k++)
+             for(k=0;k<=1;k++)
              {
+                //si no es el mismo planeta, hallamos las aceleraciones que produce cada planeta sobre otro
                 if(mod<=0.000001)
                 {
                     A[i][j][k]=-G*M[j]*R[i][j][k]/(mod*mod*mod);
@@ -68,9 +74,11 @@ void acel(double a[][2], double r[][2], double M[] ,int n)
          }
      }
 
-     for(i=0;i<=n;i++)
+
+    //Se halla la aceleración total del planeta.
+    for(i=0;i<=n;i++)
     {
-        for(k=0;k<=2;k++)
+        for(k=0;k<=1;k++)
         {
             for(j=0;j<=n;j++)
             {
@@ -79,4 +87,40 @@ void acel(double a[][2], double r[][2], double M[] ,int n)
         }
     }
 
+}
+
+void posic(double r[][1],double v[][1], double a[][1], double h,int n)
+{
+    int i,j;
+    for(i=0;i<=n;i++)
+    {
+        for(j=0;j<=1;j++)
+        {
+            r[i][j]=r[i][j]+h*v[i][j]+(h*h)/2*a[i][j];
+        }
+    }
+}
+
+void omega(double w[][1], double v[][1],double a[][1],double h, int n)
+{
+    int i,j;
+    for(i=0;i<=n;i++)
+    {
+        for(j=0;j<=1;j++)
+        {
+            w[i][j]=v[i][j]+h/2*a[i][j];
+        }
+    }
+}
+
+void veloc(double w[][1],double v[][1], double a[][1], double h, int n)
+{
+    int i,j;
+    for(i=0;i<=n;i++)
+    {
+        for(j=0;j<=1;j++)
+        {
+            v[i][j]=w[i][j]+h/2*a[i][j];
+        }
+    }
 }
--- main.cpp
+++ main.cpp
@@ -1,18 +1,17 @@
 #include "functions.h"
 #include <math.h>
 #include <stdio.h>
+const int MAXFILS=20; 
+const int MAXCOLS=20;
+const double G=6.67E-11;
+const double c=1.496E11;
+const double Ms=1.99E30;
 
 int main()
 {
-    double a[]={1,2,4,5};
-    double b[]={1,5,2,0};
-    double s[]={0,0,0,0};
-    int n=4;
-    double M;
-
-    
-    printf("Vector suma: (%lf)\n",b[0]);
-    printf("Modulo de a: %lf",M);
+    double r[MAXFILS][1],v[MAXFILS][1],a[MAXFILS][1],w[MAXFILS][1];
+    double M[MAXFILS];
+    int n;
 
     
     return 0;
